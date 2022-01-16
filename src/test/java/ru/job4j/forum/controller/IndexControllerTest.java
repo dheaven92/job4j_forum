8407645f73ef9e24fd5ab9c59d894874f2033074
Daria V <voronovadaria@gmail.com>
2022-01-16 18:21:27 +0300

    Spring test [#6881] test datasource config added

--- src/test/java/ru/job4j/forum/controller/IndexControllerTest.java
+++ src/test/java/ru/job4j/forum/controller/IndexControllerTest.java
@@ -4,9 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.job4j.forum.Application;
+import ru.job4j.forum.service.PostService;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -18,6 +20,9 @@ public class IndexControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private PostService postService;
 
     @Test
     @WithMockUser

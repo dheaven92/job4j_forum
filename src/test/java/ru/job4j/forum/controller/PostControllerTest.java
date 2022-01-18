c22bc63b032d091c3e1c423f172c723354e29bef
Daria V <voronovadaria@gmail.com>
2022-01-18 20:13:01 +0300

    Spring mock [#2096]

--- src/test/java/ru/job4j/forum/controller/PostControllerTest.java
+++ src/test/java/ru/job4j/forum/controller/PostControllerTest.java
@@ -1,6 +1,7 @@
 package ru.job4j.forum.controller;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,9 +9,15 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.job4j.forum.Application;
+import ru.job4j.forum.model.Post;
 import ru.job4j.forum.service.PostService;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -33,7 +40,6 @@ public class PostControllerTest {
                 .andExpect(view().name("post/edit"));
     }
 
-
     @Test
     @WithMockUser
     public void shouldReturnUpdateEditPageWhenLoggedIn() throws Exception {
@@ -42,6 +48,7 @@ public class PostControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(view().name("post/edit"));
     }
+
     @Test
     public void shouldReturnLoginPageWhenNotLoggedIn() throws Exception {
         this.mockMvc.perform(get("/post/create"))
@@ -50,12 +57,39 @@ public class PostControllerTest {
                 .andExpect(redirectedUrlPattern("**/login"));
     }
 
-
     @Test
     public void shouldReturnLoginPageWhenNotLoggedIn2() throws Exception {
         this.mockMvc.perform(get("/post/update"))
                 .andDo(print())
                 .andExpect(status().is3xxRedirection())
                 .andExpect(redirectedUrlPattern("**/login"));
+    }
+
+    @Test
+    @WithMockUser
+    public void shouldReturnIndexPageWhenPostSaved() throws Exception {
+        this.mockMvc.perform(post("/post/save?id=0")
+                .param("name","Куплю ладу-грант."))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"));
+        ArgumentCaptor<Post> argument = ArgumentCaptor.forClass(Post.class);
+        verify(postService).save(argument.capture(), anyInt());
+        assertThat(argument.getValue().getName(), is("Куплю ладу-грант."));
+    }
+
+    @Test
+    @WithMockUser
+    public void shouldReturnIndexPageWhenPostUpdated() throws Exception {
+        this.mockMvc.perform(post("/post/save?id=1")
+                .param("id","1")
+                .param("name","Куплю ладу-грант."))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"));
+        ArgumentCaptor<Post> argument = ArgumentCaptor.forClass(Post.class);
+        verify(postService).save(argument.capture(), anyInt());
+        assertThat(argument.getValue().getId(), is(1));
+        assertThat(argument.getValue().getName(), is("Куплю ладу-грант."));
     }
 }
--- src/test/java/ru/job4j/forum/controller/RegistrationControllerTest.java
+++ src/test/java/ru/job4j/forum/controller/RegistrationControllerTest.java
@@ -1,17 +1,25 @@
 package ru.job4j.forum.controller;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.job4j.forum.Application;
+import ru.job4j.forum.model.User;
+import ru.job4j.forum.repository.AuthorityRepository;
+import ru.job4j.forum.repository.UserRepository;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @AutoConfigureMockMvc
 @SpringBootTest(classes = Application.class)
@@ -20,6 +28,12 @@ public class RegistrationControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private UserRepository userRepository;
+
+    @MockBean
+    private AuthorityRepository authorityRepository;
+
     @Test
     @WithMockUser
     public void shouldReturnRegistrationPage() throws Exception {
@@ -27,5 +41,18 @@ public class RegistrationControllerTest {
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(view().name("auth/register"));
+    }
+
+    @Test
+    public void shouldReturnLoginPageWhenUserRegistered() throws Exception {
+        this.mockMvc.perform(post("/register")
+                .param("username","Алекс")
+                .param("password","123"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/login"));
+        ArgumentCaptor<User> argument = ArgumentCaptor.forClass(User.class);
+        verify(userRepository).save(argument.capture());
+        assertThat(argument.getValue().getUsername(), is("Алекс"));
     }
 }
